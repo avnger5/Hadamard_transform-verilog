114518e9ea2d9842c370719037632cfa862d1342
Avinash V <47024198+avnger5@users.noreply.github.com>
2022-04-16 10:29:54 +0530

    Add files via upload

---
+++ Linear_systolic_hadamard4pt.v
@@ -0,0 +1,108 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:43:18 03/22/2022 
+// Design Name: 
+// Module Name:    Linear_systolic_hadamard4pt 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Linear_systolic_hadamard4pt(clk,start,x0,x1,x2,x3,y0);
+
+input clk;
+input start;
+input signed [8:0] x0,x1,x2,x3;
+output reg signed [11:0] y0;
+
+reg signed [9:0] p1;
+reg signed [10:0] p2;
+reg tag1,tag2,tag3;
+reg [1:0] t;
+
+initial
+begin
+tag2=1'b0;
+tag1=1'b1;
+end
+
+always@(posedge clk)
+begin
+	if(start)
+	begin
+	if(1)
+		begin
+			tag1 <= ~tag1;
+		end
+	if(tag1)
+		begin
+			tag2 <= ~tag2;
+		end
+	
+	tag3 <= tag2^tag1;
+	end
+	if(start)
+		begin
+			if(tag1)
+				begin
+					p1 <= x0 + x1;
+				end
+			else
+				begin
+					p1 <= x0 - x1 ;
+				end
+		end
+	else
+		begin
+			y0 <= 1'b0;
+		end
+		
+	if(start)
+		begin
+			if(tag2)
+				begin
+					p2 <= p1 + x2;
+				end
+			else
+				begin
+					p2 <= p1 - x2;
+				end
+		end
+	else
+		begin
+			y0 <= 1'b0;
+		end
+		
+	if(start)
+		begin
+			if(tag3)
+				begin
+					y0 <= p2 + x3;
+				end
+			else
+				begin
+					y0 <= p2 - x3;
+				end
+		end
+	else
+		begin
+			y0 <= 1'b0;
+		end
+	if(!start)
+	begin
+		tag2<=1'b0;
+		tag1<=1'b1;
+	end
+end
+
+endmodule
---
+++ hadamard_linear_test.v
@@ -0,0 +1,105 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   16:46:57 03/23/2022
+// Design Name:   Linear_systolic_hadamard4pt
+// Module Name:   C:/Users/hp/Desktop/DSP architecture/Project/Linearsystolic/hadamard_linear_test.v
+// Project Name:  Linearsystolic
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Linear_systolic_hadamard4pt
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module hadamard_linear_test;
+
+	// Inputs
+	reg clk;
+	reg start;
+
+	reg signed [8:0] x0;
+	reg signed [8:0] x1;
+	reg signed [8:0] x2;
+	reg signed [8:0] x3;
+
+	// Outputs
+	wire signed [11:0] y0;
+
+	// Instantiate the Unit Under Test (UUT)
+	Linear_systolic_hadamard4pt uut (
+		.clk(clk), 
+		.start(start), 
+		.x0(x0), 
+		.x1(x1), 
+		.x2(x2), 
+		.x3(x3), 
+		.y0(y0)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		start = 1;
+		x0 = 3;
+		x1 = 4;
+		x2 = -2;
+		x3 = 1;
+
+		#200;
+		
+		start = 0;
+		#20
+		x0 = -1;
+		x1 = 6;
+		x2 = 2;
+		x3 = 4;
+		
+
+		start = 1;
+
+		#200;
+		
+		start = 0;
+		#20
+		x0 = 5;
+		x1 = -3;
+		x2 = 1;
+		x3 = 0;
+		
+		start = 1;
+		
+		#200;
+		
+		start = 0;
+		#20
+		x0 = -2;
+		x1 = -3;
+		x2 = -1;
+		x3 = -5;
+		
+		start = 1;
+
+		
+
+	end
+	
+	always #10 clk = ~clk;
+	
+	initial
+		begin
+			$monitor($time," X0 = %d ,X1 = %d ,X2 = %d ,X3 = %d ,  Y = %d  ",x0,x1,x2,x3,y0);
+		end
+			
+endmodule
+
